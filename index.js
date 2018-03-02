22dab2fc9cadf7b5a5845f23c08aa051cb36515b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 22:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (array) {
   const newarray = array.map(function (element) {
+    console.log(element);
     return element[0] && element[1]
   })
   return newarray
-  
 }
