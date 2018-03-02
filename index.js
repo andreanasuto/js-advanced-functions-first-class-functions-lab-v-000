2e994b48fb97dfa135a125a876b16c341769825c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 22:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers() = function (array) {
+const returnFirstTwoDrivers = function (array) {
   const newarray = [array[0], array[1]]
   return newarray
 }
 
-const returnLastTwoDrivers() = function (array) {
+const returnLastTwoDrivers = function (array) {
   const newarray = [array[array.length - 2], array[array.length - 1]]
   return newarray
 }
