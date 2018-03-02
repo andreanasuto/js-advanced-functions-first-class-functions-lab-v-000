0330c80dbc2202c0998930aa6f14e56108396e29
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 22:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const returnFirstTwoDrivers = function (array) {
   const newarray = [array[0], array[1]]
   return newarray
 }
+
+const returnLastTwoDrivers = function (array) {
+  const newarray = [array[array.length - 2], array[array.length - 1]]
+
+}
